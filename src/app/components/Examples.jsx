19b0393b9921e873b62dd3d3928856a3584b4420
reacton <aliyasin.emir16@gmail.com>
2024-02-14 22:49:28 +0300

    Refactored the tab section

--- src/app/components/Examples.jsx
+++ src/app/components/Examples.jsx
@@ -4,10 +4,34 @@ import { EXAMPLES } from "@/app/data";
 import { useState } from "react";
 
 const Examples = () => {
-  const [tabName, setTabName] = useState('components');
+  const initialCode = (
+    <span className="text-[#e7c7ff]">Please select the tab</span>
+  );
+  const [tabName, setTabName] = useState("");
+  const [tabContent, setTabContent] = useState(initialCode);
 
   const handleClick = (tabName) => {
+    const tabContentCode = (
+      <div>
+        <h3 className="text-lg text-gray-100 font-semibold">
+          {EXAMPLES[tabName].title}
+        </h3>
+        <p className="text-lg text-[#dfb4ff]">
+          {EXAMPLES[tabName].description}
+        </p>
+        <span
+          className={`${
+            tabName === "components" || tabName === "jsx" || tabName === "props"
+              ? "text-lg"
+              : ""
+          } text-gray-200 whitespace-pre-wrap font-light`}
+        >
+          {EXAMPLES[tabName].code}
+        </span>
+      </div>
+    );
     setTabName(tabName);
+    setTabContent(tabContentCode);
   };
   return (
     <div className="w-[1240px]">
@@ -36,13 +60,11 @@ const Examples = () => {
             onClick={(tabName) => handleClick("state")}
             className="bg-[#ba90db] hover:bg-[#3e2453] antialiased hover:text-[#ba90db] cursor-pointer px-6 rounded-t-lg"
           >
-            State   
+            State
           </h2>
         </div>
-        <div className='w-[1240px] flex flex-col gap-4 rounded-b-lg rounded-tr-lg p-5 bg-[#7b549b] justify-center px-6'>
-          <h3 className="text-lg text-gray-100 font-semibold">{EXAMPLES[tabName].title}</h3>
-          <p className="text-lg text-[#dfb4ff]">{EXAMPLES[tabName].description}</p>
-          <span className={`${tabName === 'components' || tabName === 'jsx' || tabName === 'props' ? 'text-lg' : "text-"} text-gray-200 whitespace-pre-wrap font-light`}>{EXAMPLES[tabName].code}</span>
+        <div className="w-[1240px] flex flex-col gap-4 rounded-b-lg rounded-tr-lg p-5 bg-[#7b549b] justify-center px-6">
+          {tabContent}
         </div>
       </div>
     </div>
